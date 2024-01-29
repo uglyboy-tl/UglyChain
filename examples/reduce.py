e574954bd112dc9b76d0b2328e84e0f9a560c042
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-01-29 16:33:08 +0800

    Add reduce_function to reduce.py

--- examples/reduce.py
+++ examples/reduce.py
@@ -1,7 +1,8 @@
+from typing import Any
+
 from loguru import logger
 
 from uglychain import ReduceChain, Model
-
 
 def reduce(model: Model | None = None):
     if model:
@@ -16,5 +17,26 @@ def reduce(model: Model | None = None):
     logger.info(llm(input=input, history="锄禾日当午"))
 
 
+def reduce_function(model: Model | None = None):
+    def string(obj: Any) -> str:
+        """Converts an object to a string.
+
+        Args:
+            obj (Any): The object to be converted.
+        """
+        return str(obj)
+    tools = [string]
+    if model:
+        llm = ReduceChain(model=model, tools=tools)
+    else:
+        llm = ReduceChain(tools=tools)
+    input = [
+        "请写出这句诗的下一句",
+        "请写出这句诗的下一句",
+        "请写出这句诗的下一句",
+    ]
+    logger.info(llm(input=input, history="锄禾日当午"))
+
 if __name__ == "__main__":
     reduce(Model.YI)
+    reduce_function(Model.GPT4_TURBO)
