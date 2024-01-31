1253203dbf6455166f628ff8592e6690fb002717
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-01-31 14:29:35 +0800

    Add ReActChain and test cases for ReActChain

---
+++ tests/chains/test_react.py
@@ -0,0 +1,48 @@
+from enum import Enum
+from typing import Callable, List
+
+import pytest
+
+from uglychain import Model
+from uglychain.chains.react_bad import ReActChain
+
+
+class Unit(Enum):
+    FAHRENHEIT = "fahrenheit"
+    CELSIUS = "celsius"
+
+
+def get_current_weather(location: str, unit: Unit = Unit.FAHRENHEIT) -> str:
+    """Get the current weather in a given location.
+
+    Args:
+        location (str): The city and state, e.g., San Francisco, CA
+        unit (Unit): The unit to use, e.g., fahrenheit or celsius
+    """
+    return "晴天，25华氏度"
+
+
+def search_baidu(query: str) -> str:
+    """Search Baidu for the query.
+
+    Args:
+        query (str): The query to search.
+    """
+    return "牛顿出生于1642年"
+
+
+@pytest.mark.parametrize("model", [Model.GPT3_TURBO, Model.YI_32K, Model.QWEN, Model.GLM3])
+def test_react1(model):
+    tools: List[Callable] = [get_current_weather, search_baidu]
+    llm = ReActChain(tools=tools)
+
+    response = llm("What's the weather in San Francisco?")
+    assert response.find("25") >= 0
+
+@pytest.mark.parametrize("model", [Model.GPT3_TURBO, Model.YI_32K, Model.QWEN, Model.GLM3])
+def test_react2(model):
+    tools: List[Callable] = [get_current_weather, search_baidu]
+    llm = ReActChain(tools=tools)
+
+    response = llm("牛顿生于哪一年")
+    assert response.find("1642") >= 0 or response.find("1643") >= 0
---
+++ uglychain/chains/react_bad.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8-*-
+
+from dataclasses import dataclass, field
+from typing import Callable, Dict, List, Union
+
+from loguru import logger
+
+from uglychain.llm.tools import ActionResopnse, tools_schema
+
+from .llm import LLM, FunctionCall, GenericResponseType
+
+REACT_PROMPT = """
+Assistant is a large language model trained by OpenAI.
+
+Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
+
+TOOLS:
+------
+
+Assistant has access to the following tools:
+
+{tools}
+
+To use a tool, please use the following format:
+
+```
+Thought: Do I need to use a tool? Yes
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+```
+
+When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+
+```
+Thought: Do I need to use a tool? No
+Final Answer: [your response here]
+```
+
+Begin!
+
+New input:
+{input}
+
+{react_history}
+"""
+
+
+def finish(answer: str) -> str:
+    """returns the answer and finishes the task.
+    Args:
+        answer (str): The response to return.
+    """
+    return answer
+
+
+def call(tools: List[Callable], response: FunctionCall):
+    for tool in tools:
+        if tool.__name__ == response.name:
+            return tool(**response.args)
+    raise ValueError(f"Can't find tool {response.name}")
+
+
+@dataclass
+class Action:
+    thought: str
+    action: str
+    params: Dict
+    obs: str
+    current: bool = True
+
+    @property
+    def done(self) -> bool:
+        if self.action == "finish":
+            return True
+        else:
+            return False
+
+    def __str__(self) -> str:
+        return self.info
+
+    @property
+    def info(self) -> str:
+        return f"Thought: {self.thought}\nAction: {self.action}\nAction Input: {self.params}\nObservation: {self.obs}"
+
+
+@dataclass
+class ReActChain(LLM[GenericResponseType]):
+    llmchain: LLM = field(init=False)
+
+    def __post_init__(self):
+        self._acts = []
+        # super().__post_init__()
+        if self.system_prompt:
+            self.llm.set_system_prompt(self.system_prompt)
+        self.prompt = self.prompt_template
+        assert self.tools is not None, "tools must be set"
+        self.tools.insert(0, finish)
+        self.tools_schema=tools_schema(self.tools)
+        self.tool_names=[tool.__name__ for tool in self.tools]
+
+        self.llmchain = LLM(REACT_PROMPT, self.model)
+        self.formatchain = LLM(model=self.model,tools=self.tools, response_model=ActionResopnse)
+        self.formatchain.llm.use_native_tools = False
+
+    def _call(self, inputs: Dict[str, str]) -> Union[str, GenericResponseType]:
+        input = self.prompt.format(**inputs)
+        assert self.tools is not None, "tools must be set"
+        react_response = self.llmchain(input=input, react_history="", tools = self.tools_schema, tool_names=self.tool_names)
+        logger.debug(f"{react_response}")
+        response = self.formatchain(react_response)
+        thought = response.thought
+        action = response.action.name
+        params = response.action.args
+        obs = call(self.tools, response.action)
+        act = Action(thought, action, params, obs)
+        logger.success(act.info)
+        while not act.done:
+            if self._acts:
+                self._acts[-1].current = False
+            self._acts.append(act)
+            react_history = (
+                "\n".join(str(a) for a in self._acts)
+            )
+            logger.debug(f"{input}\n{react_history}")
+            react_response = self.llmchain(input=input, react_history=react_history, tools = self.tools_schema, tool_names=self.tool_names)
+            logger.debug(f"{react_response}")
+            response = self.formatchain(react_response)
+            thought = response.thought
+            action = response.action.name
+            params = response.action.args
+            obs = call(self.tools, response.action)
+            act = Action(thought, action, params, obs)
+            logger.success(act.info)
+        response = obs
+        if self.response_model:
+            llm = LLM(model=self.model, response_model=self.response_model)
+            return llm(response)
+        else:
+            return response
