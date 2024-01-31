d2ca5acd4f7a4c4f9b7a5f9bd55c7b79853a8996
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-01-31 16:05:26 +0800

    Add react.py with ReActChain and example functions

---
+++ examples/react.py
@@ -0,0 +1,47 @@
+from enum import Enum
+from typing import Callable, List
+
+from loguru import logger
+
+from uglychain import Model
+from uglychain.chains.react_bad import ReActChain
+
+
+class Unit(Enum):
+    FAHRENHEIT = "fahrenheit"
+    CELSIUS = "celsius"
+
+
+def get_current_weather(location: str, unit: Unit = Unit.FAHRENHEIT) -> str:
+    """Get the current weather in a given location.
+
+    Args:
+        location (str): The city and state, e.g., San Francisco, CA
+        unit (Unit): The unit to use, e.g., fahrenheit or celsius
+    """
+    return "晴天，25华氏度"
+
+
+def search_baidu(query: str) -> str:
+    """Search Baidu for the query.
+
+    Args:
+        query (str): The query to search.
+    """
+    return "牛顿出生于1642年"
+
+
+def react(model: Model | None = None):
+    tools: List[Callable] = [get_current_weather, search_baidu]
+    if model:
+        llm = ReActChain(model=model, tools=tools)
+    else:
+        llm = ReActChain(tools=tools)
+
+    response = llm("牛顿生于哪一年")
+    # response = llm("What's the weather in San Francisco?")
+    logger.info(response)
+
+
+if __name__ == "__main__":
+    react()
