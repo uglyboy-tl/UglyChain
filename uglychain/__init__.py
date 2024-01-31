1bb1bf4fedce9ce2e0f80dd9be625f5f576a49aa
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-01-31 18:14:12 +0800

    Update version to 0.0.4

--- uglychain/__init__.py
+++ uglychain/__init__.py
@@ -2,7 +2,7 @@ from .chains import LLM, MapChain, ReAct, ReActChain, ReduceChain
 from .llm import BaseLanguageModel, Model
 from .retrievers import BaseRetriever, StoresRetriever
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 __all__ = [
     "BaseLanguageModel",
