426353771a8bbe5a1403238437af222a9c329f87
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-02-05 19:00:56 +0800

    Add tests for Baichuan provider

---
+++ tests/provider/test_baichuan.py
@@ -0,0 +1,19 @@
+import pytest
+
+from examples.schema import Gender, UserDetail
+from uglychain import LLM, Model
+
+
+@pytest.mark.parametrize("model", [Model.BAICHUAN_TURBO, Model.BAICHUAN_PRO])
+def test_llm(model):
+    llm = LLM(model=model)
+    assert isinstance(llm("你是谁？"), str)
+
+
+@pytest.mark.parametrize("model", [Model.BAICHUAN_TURBO, Model.BAICHUAN_PRO])
+def test_instructor(model):
+    llm = LLM(model=model, response_model=UserDetail)
+    obj = llm("Extract Jason is a boy")
+    assert isinstance(obj, UserDetail)
+    assert obj.name == "Jason"
+    assert obj.gender == Gender.MALE
