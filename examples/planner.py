969a321714cb814caab430ade06907aab393bdab
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-02-06 11:09:23 +0800

    Update planner.py with additional functionality

--- examples/planner.py
+++ examples/planner.py
@@ -3,8 +3,8 @@ import sys
 from loguru import logger
 
 from uglychain import Model
-from uglychain.tools import run_code, search_knowledgebase
-from uglychain.woker.planner import Planner
+from uglychain.tools import run_code, search_knowledgebase, text_completion
+from uglychain.woker.planner import Planner, Tasks
 
 logger.remove()
 logger.add(sink=sys.stdout, level="TRACE")
@@ -12,15 +12,32 @@ logger.add(sink=sys.stdout, level="TRACE")
 
 def planner(model: Model | None = None):
     objective = "安装零一万物的 YI-32K 模型"
+    # objective = "让世界更加和平"
     tools = [search_knowledgebase, run_code]
     if model:
         worker = Planner(model=model, tools=tools)
     else:
         worker = Planner(tools=tools)
-    tasks = worker.run(objective).tasks
-    for task in tasks:
-        logger.info(task)
+    tasks = worker.run(objective)
+    # for task in tasks.tasks:
+    #    logger.info(task)
+    logger.info(tasks.pretty)
+    return tasks
+
+
+def run_single_task(tasks: Tasks):
+    def execute(params: str, dependent_task_outputs: str, objective: str) -> str:
+        prompt = f"Complete your assigned task based on the objective and only based on information provided in the dependent task output, if provided. \n###\nYour objective: {objective}. \n###\nYour task: {params} \n###\nDependent tasks output: {dependent_task_outputs}  \n###\nYour task: {params}\n###\nRESPONSE"
+        return text_completion(prompt)
+
+    task = tasks.tasks[0]
+    task_output = tasks.execute_task(task, execute, "安装零一万物的 YI-32K 模型")
+    tools = [search_knowledgebase, run_code]
+    worker = Planner(tools=tools)
+    tasks = worker.run(tasks=tasks, task_output=task_output)
+    logger.info(tasks.pretty)
 
 
 if __name__ == "__main__":
-    planner(Model.YI)
+    tasks = planner(Model.YI)
+    run_single_task(tasks)
--- uglychain/woker/planner.py
+++ uglychain/woker/planner.py
@@ -1,7 +1,8 @@
+import inspect
 from dataclasses import dataclass, field
 from typing import Callable, List, Optional, Union, cast
 
-from loguru import logger
+from docstring_parser import parse
 from pydantic import BaseModel, Field
 
 from uglychain import LLM
@@ -11,22 +12,41 @@ from .base import BaseWorker
 
 ROLE = "You are an expert task manager AI."
 
-TASK_MANAGER_PROMPT = """You have to tasked with cleaning the formatting of and reprioritizing the tasks `{minified_task_list}`.
+CREATE_TASKS_PROMPT = """You are an expert task list creation AI tasked with creating a list of tasks, considering the ultimate objective of your team: {objective}.
+Create a very short task list based on the objective, the final output of the last task will be provided back to the user. Limit tasks types to those that can be completed with the available tools listed below. Task description should be detailed.
 
-Consider the Ultimate Objective: {objective}
+### AVAILABLE TOOLS: {tools_info}.
 
-You need to follow these rules:
-- **Do not remove any tasks**.
-- Create new tasks based on the result of last task if necessary for the objective. Limit tasks types to those that can be completed with the available tools listed below. Task description should be detailed. Current tool option is {tool_names} and only.
-- Do not reorder completed tasks. Only reorder and dedupe incomplete tasks.
+### RULES:
+- The final task should always output the final result of the overall objective.
 - Make sure all task IDs are in chronological order.
-- The last task is always to provide a final summary report including tasks executed and summary of knowledge acquired.
+- Helpful Notes as guidance: "{notes}"
+- no more than 5 tasks should be created.
 - task descriptions should be in Chinese.
-
-The last completed task has the following result: {result}.
 """
 
-YOUR_FIRST_TASK = "制定一份不超过5条的任务清单"
+TASK_MANAGER_PROMPT = """
+You are a task management AI tasked with reprioritizing the a list of tasks, considering the ultimate objective of your team: {objective}. Create new tasks based on the result of last task if necessary for the objective. Limit tasks types to those that can be completed with the available tools listed below. Task description should be detailed.
+
+### AVAILABLE TOOLS: {tools_info}.
+
+### RULES:
+- Do not remove any tasks.
+- The maximum task list length is 7. Do not add an 8th task.
+- Do not reorder completed tasks. Only reorder and dedupe incomplete tasks.
+- Make sure all task IDs are in chronological order.
+- The last step is always to provide a final summary report of all tasks.
+
+### Here is the current task list: {tasks}
+### The last completed task has the following result: {task_output}.
+"""
+
+
+def get_function_description(func: Callable) -> str:
+    try:
+        return parse(inspect.getdoc(func)).short_description  # type: ignore
+    except AttributeError:
+        return ""
 
 
 class Task(BaseModel):
@@ -36,94 +56,65 @@ class Task(BaseModel):
         default_factory=list,
         description="The task ids that this task is dependent on. It should always be an empty array, or an array of int representing the task ID it should pull results from.",
     )
-
-
-class Task_Completed(Task):
-    completed: bool = True
-    result: Optional[str] = Field(
-        default=None, description="The task result. If the task is not completed, this field should be empty."
-    )
-    result_summary: Optional[str] = Field(
-        default=None, description="The task result summary. If the task is not completed, this field should be empty."
+    completed: bool = Field(
+        default=False,
+        description="The task completion status.",
     )
 
-    @classmethod
-    def from_task(cls, task: Task, result: Optional[str], result_summary: Optional[str]) -> "Task_Completed":
-        return cls(
-            id=task.id,
-            task=task.task,
-            dependent_task_ids=task.dependent_task_ids,
-            result=result,
-            result_summary=result_summary,
-        )
+    @property
+    def result(self) -> str:
+        if not hasattr(self, "_result"):
+            self._result = ""
+        return self._result
+
+    @result.setter
+    def result(self, value: str):
+        self._result = value
 
 
 class Tasks(BaseModel):
-    objective: str = Field(..., description="The ultimate objective.")
     tasks: List[Task] = Field(
         ...,
         description="The list of tasks.",
     )
 
-    @property
-    def incomplete_tasks(self) -> List[Task]:
-        incomplete_tasks = [task for task in self.tasks if not hasattr(task, "result")]
-        incomplete_tasks.sort(key=lambda x: x.id)
-        return incomplete_tasks
+    def get_task_by_id(self, task_id: int) -> Task:
+        try:
+            return next(task for task in self.tasks if task.id == task_id)
+        except StopIteration as err:
+            raise ValueError(f"Task with id {task_id} not found.") from err
 
-    @property
-    def completed_tasks(self) -> List[Task]:
-        completed_tasks = [task for task in self.tasks if hasattr(task, "result")]
-        completed_tasks.sort(key=lambda x: x.id)
-        return completed_tasks
+    def execute_task(self, task: Union[Task, int], execute: Callable[[str, str, str], str], objective: str):
+        if isinstance(task, int):
+            task = self.get_task_by_id(task)
+        assert not task.completed, f"Task {task.id} is already completed."
+        dependent_tasks = [self.get_task_by_id(dependent_task_id) for dependent_task_id in task.dependent_task_ids]
+        assert all(
+            dependent_task.completed for dependent_task in dependent_tasks
+        ), f"Dependent tasks for task {task.id} are not completed."
+        dependent_task_outputs = "\n".join(dependent_task.result for dependent_task in dependent_tasks)
+        result = execute(task.task, dependent_task_outputs, objective)
+        task.completed = True
+        task.result = result
+        return result
 
-    @property
-    def tasks_completed(self) -> bool:
-        return all(hasattr(task, "result") for task in self.tasks)
+    def reorder_tasks(self):
+        self.tasks.sort(key=lambda x: x.id)
 
-    def get_task_by_id(self, task_id: int) -> Optional[Task]:
+    def __str__(self):
+        tasks = []
         for task in self.tasks:
-            if task.id == task_id:
-                return task
-        return None
+            task_dict = task.model_dump()
+            task_dict["completed"] = task.completed
+            tasks.append(task_dict)
+        return str(tasks)
 
-    @classmethod
-    def from_list(cls, objective: str, task_list: List[str]) -> "Tasks":
-        tasks = [Task(id=i + 1, task=task) for i, task in enumerate(task_list)]
-        return cls(objective=objective, tasks=tasks)
-
-
-def execute_task(
-    tasks: Tasks,
-    execute: Callable[[str], str],
-    summarizer_agent: Optional[Callable[[str], str]] = None,
-    objective: str = "",
-) -> None:
-    incomplete_tasks = tasks.incomplete_tasks
-    task = incomplete_tasks[0]
-    if task.dependent_task_ids:
-        all_dependent_tasks_complete = True
-        for dep_id in task.dependent_task_ids:
-            dependent_task = tasks.get_task_by_id(dep_id)
-            if not dependent_task or not hasattr(dependent_task, "result"):
-                all_dependent_tasks_complete = False
-                break
-        if not all_dependent_tasks_complete:
-            logger.warning(f"Task {task.id} is dependent on other tasks.")
-
-    if objective:
-        task_prompt = f"Complete your assigned task based on the objective: {objective}. Your task: {task.task}"
-    else:
-        task_prompt = task.task
-    result = execute(task_prompt)
-    result_summary = None
-    if summarizer_agent:
-        result_summary = summarizer_agent(result)
-    try:
-        index = tasks.tasks.index(task)
-        tasks.tasks[index] = Task_Completed.from_task(task, result, result_summary)
-    except ValueError:
-        pass  # 旧值不在列表中
+    @property
+    def pretty(self):
+        output = "\n"
+        for task in self.tasks:
+            output += f"{'✓' if task.completed else '✗'} {task.id}. {task.task} [ {','.join(str(i) for i in task.dependent_task_ids)} ]\n"
+        return output
 
 
 @dataclass
@@ -135,54 +126,29 @@ class Planner(BaseWorker):
     llm: LLM = field(init=False)
 
     def __post_init__(self):
-        self.llm = LLM(TASK_MANAGER_PROMPT, self.model, self.role, Tasks)
+        self.llm = LLM(CREATE_TASKS_PROMPT, self.model, self.role, Tasks)
         try:
             self.tools.index(text_completion)
         except ValueError:
             self.tools.insert(0, text_completion)
-        self.tool_names = ", ".join([f"`{tool.__name__}`" for tool in self.tools])
+        self.tools_info = ", ".join([f"`{tool.__name__}`:'{get_function_description(tool)}'" for tool in self.tools])
 
     def run(
         self,
         objective: str = "",
         tasks: Optional[Union[Tasks, List[str]]] = None,
+        task_output: str = "",
     ):
-        if self.objective and objective and self.objective != objective:
-            logger.warning(f"Objective has different values: {self.objective} and {objective}. use the original one.")
-        elif objective:
-            self.objective = objective
-        assert self.objective, "Objective is required."
-
-        if tasks is None:
-            first_task = Task(id=1, task=YOUR_FIRST_TASK)
-            tasks = Tasks(objective=self.objective, tasks=[first_task])
-            execute_task(tasks, text_completion, objective=self.objective)
-        elif isinstance(tasks, list):
-            tasks = Tasks.from_list(self.objective, tasks)
-            return tasks
-        completed_tasks = tasks.completed_tasks
-        task = cast(Task_Completed, completed_tasks[0])
-        result = task.result
-        original_task_list = tasks.tasks.copy()
-        minified_task_list = [
-            {k: v for k, v in task.model_dump().items() if k != "result" and k != "result_summary"}
-            for task in tasks.tasks
-        ]
-        tasks = self.llm(
-            objective=self.objective, minified_task_list=minified_task_list, tool_names=self.tool_names, result=result
-        )
-        tasks = cast(Tasks, tasks)
-        if tasks.objective != self.objective:
-            logger.warning(f"Objective has been changed from {self.objective} to {tasks.objective}. fix it.")
-            tasks.objective = self.objective
-        for i, updated_task, original_task in zip(
-            range(len(original_task_list)), tasks.tasks, original_task_list, strict=False
-        ):
-            if updated_task.task != original_task.task:
-                logger.warning(f"Task {updated_task.id} has been changed. use the new one.")
-                original_task.task = updated_task.task
-            if isinstance(original_task, Task_Completed):
-                tasks.tasks[i] = original_task
-        if self.storage:
-            self.storage.save(tasks)
-        return tasks
+        if tasks is None or isinstance(tasks, list):
+            assert not task_output, "task_output should be empty if tasks is a list."
+            self.llm.prompt = CREATE_TASKS_PROMPT
+            if tasks:
+                notes = ";".join(f"{i+1}. {item}" for i, item in enumerate(tasks))
+            else:
+                notes = ""
+            response = self._ask(objective=objective, tools_info=self.tools_info, notes=notes)
+            return cast(Tasks, response)
+        else:
+            self.llm.prompt = TASK_MANAGER_PROMPT
+            response = self._ask(objective=objective, tools_info=self.tools_info, task_output=task_output, tasks=tasks)
+            return cast(Tasks, response)
