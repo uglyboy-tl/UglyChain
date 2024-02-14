1bda640688aff025113a98f9785e7cb42f736ab0
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-02-14 18:10:58 +0800

    Refactor conditional statement in ChatGPT class

--- uglychain/provider/chatgpt.py
+++ uglychain/provider/chatgpt.py
@@ -26,11 +26,16 @@ class ChatGPT(ChatGPTAPI):
         stop: Union[Optional[str], List[str]] = None,
     ) -> str:
         kwargs = self.get_kwargs(prompt, response_model, tools, stop)
-        if response_model and self.model in [
-            "gpt-3.5-turbo-0125",
-            "gpt-3.5-turbo",
-            "gpt-4-turbo-preview",
-        ]:
+        if (
+            response_model
+            and not self.use_native_tools
+            and self.model
+            in [
+                "gpt-3.5-turbo-0125",
+                "gpt-3.5-turbo",
+                "gpt-4-turbo-preview",
+            ]
+        ):
             kwargs["response_format"] = {"type": "json_object"}
         try:
             response = self.completion_with_backoff(**kwargs)
