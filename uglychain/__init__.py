f4b174326418f3b53d59246cb8ae00ab6d6282e3
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-02-19 20:53:00 +0800

    Update version number in pyproject.toml and __init__.py

--- uglychain/__init__.py
+++ uglychain/__init__.py
@@ -3,7 +3,7 @@ from .llm import BaseLanguageModel, finish, run_function
 from .retrievers import BaseRetriever, Retriever, StorageRetriever
 from .worker import BaseWorker
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 __all__ = [
     "BaseLanguageModel",
