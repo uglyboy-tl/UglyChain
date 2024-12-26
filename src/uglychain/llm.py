1ef4d8a4268018033d4bc1231dde400d021e64bb
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-12-26 13:41:34 +0800

    refactor(uglychain): 重构代码以提高可维护性和扩展性
    - 重构了 llm 函数，优化了参数处理和模型调用逻辑
    - 新增 ResponseFormatter 类，用于处理响应格式化相关功能
    - 改进了错误处理和日志记录，提高了代码的健壮性
    - 优化了代码结构，提高了可读性和可维护性

--- src/uglychain/llm.py
+++ src/uglychain/llm.py
@@ -7,7 +7,7 @@ from typing import Any, TypeVar, cast, get_type_hints
 import aisuite
 from pydantic import BaseModel
 
-from .response_format import from_response, get_response_format_prompt
+from .response_format import ResponseFormatter
 
 # 创建一个泛型变量，用于约束BaseModel的子类
 T = TypeVar("T", str, BaseModel)
@@ -17,15 +17,73 @@ _client_instance = None
 
 
 def get_client() -> aisuite.Client:
+    """
+    返回一个aisuite的Client实例。
+
+    该函数使用了单例模式的设计原则，确保在整个应用程序中只有一个Client实例被创建和使用。
+    这样做可以节省资源，并确保与Client的所有交互都是通过同一个实例进行的。
+
+    Returns:
+        aisuite.Client: aisuite的Client实例。
+    """
+    # 使用全局变量_client_instance来存储Client的唯一实例
     global _client_instance
+
+    # 检查_client_instance是否已经初始化
     if _client_instance is None:
+        # 如果尚未初始化，则创建一个新的Client实例并赋值给_client_instance
         _client_instance = aisuite.Client()
+
+    # 返回Client实例
     return _client_instance
 
 
 def empty_client() -> None:
-    global _client_instance
-    _client_instance = None
+    """
+    将客户端实例重置为 None。
+
+    该函数用于清除全局客户端实例变量，
+    确保客户端状态在后续配置或使用前被重置。
+    """
+    global _client_instance  # 声明要修改的全局变量
+    _client_instance = None  # 将客户端实例重置为 None
+
+
+def _get_messages(prompt_ret: str | list[dict[str, str]], prompt: Callable) -> list[dict[str, str]]:
+    """
+    获取消息列表，用于模型推理。
+
+    :param prompt_ret: 提示函数的返回值，可以是字符串或消息列表
+    :param prompt: 提示函数
+    :return: 返回消息列表
+    """
+    if isinstance(prompt_ret, str):
+        messages = []
+        if prompt.__doc__ and prompt.__doc__.strip():
+            messages.append({"role": "system", "content": prompt.__doc__})
+        messages.append({"role": "user", "content": prompt_ret})
+        return messages
+    elif isinstance(prompt_ret, list):
+        return prompt_ret
+    else:
+        raise TypeError("Expected prompt_ret to be a str or list of Messages")
+
+
+def _process_structured_with_parameters(
+    messages: list[dict[str, str]], merged_api_params: dict[str, Any], return_type: type[BaseModel]
+):
+    # TODO: 可以选择用怎样的方式实现结构化输出，当前只实现了基于 Prompt 的方式
+    system_message = messages[0]
+    if system_message["role"] == "system":
+        system_message["content"] += "\n-----\n" + ResponseFormatter.get_response_format_prompt(return_type)
+    else:
+        system_message = {
+            "role": "system",
+            "content": ResponseFormatter.get_response_format_prompt(return_type),
+        }
+        messages.insert(0, system_message)
+
+    merged_api_params["response_format"] = {"type": "json_object"}
 
 
 def llm(model: str, **api_params: Any) -> Callable[[Callable[..., T]], Callable[..., T | list[T]]]:
@@ -68,37 +126,38 @@ def llm(model: str, **api_params: Any) -> Callable[[Callable[..., T]], Callable[
             if return_type is not str and not issubclass(return_type, BaseModel):
                 raise TypeError(f"Unsupported return type: {return_type}")
 
-            # prompt -> str
+            # 获取提示函数的返回值
             res = cast(str | list, prompt(*prompt_args, **prompt_kwargs))
-            # Convert prompt into messages
+            # 获取提示函数的返回值对应的消息列表
             messages = _get_messages(res, prompt)
 
+            # 合并装饰器级别的API参数和函数级别的API参数
             merged_api_params = default_api_params_from_decorator.copy()
             if api_params:
                 merged_api_params.update(api_params)
 
-            if not issubclass(return_type, str):
-                # TODO: 可以选择用怎样的方式实现结构化输出，当前只实现了基于 Prompt 的方式
-                system_message = messages[0]
-                if system_message["role"] == "system":
-                    system_message["content"] += "\n-----\n" + get_response_format_prompt(return_type)
-                else:
-                    system_message = {"role": "system", "content": get_response_format_prompt(return_type)}
-                    messages.insert(0, system_message)
-
+            # 获取同时运行的次数
             n = merged_api_params.get("n", 1)
+
+            # 处理结构化输出
+            if not issubclass(return_type, str):
+                _process_structured_with_parameters(messages, merged_api_params, return_type)
+
+            # 调用模型
             response = get_client().chat.completions.create(
                 model=merged_api_params.pop("model", default_model_from_decorator),
                 messages=messages,
                 **merged_api_params,
             )
 
+            # 处理返回值
             result = []
-            for choice in response.choices:
-                if return_type is str:
+            if return_type is str:
+                for choice in response.choices:
                     result.append(choice.message.content)
-                elif issubclass(return_type, BaseModel):
-                    result.append(from_response(return_type, choice.message.content))
+            elif issubclass(return_type, BaseModel):
+                for choice in response.choices:
+                    result.append(ResponseFormatter.parse_model_from_response(return_type, choice.message.content))
 
             if len(result) <= 0:
                 raise ValueError("No choices returned from the model")
@@ -115,23 +174,3 @@ def llm(model: str, **api_params: Any) -> Callable[[Callable[..., T]], Callable[
         return model_call
 
     return parameterized_lm_decorator
-
-
-def _get_messages(prompt_ret: str | list[dict[str, str]], prompt: Callable) -> list[dict[str, str]]:
-    """
-    获取消息列表，用于模型推理。
-
-    :param prompt_ret: 提示函数的返回值，可以是字符串或消息列表
-    :param prompt: 提示函数
-    :return: 返回消息列表
-    """
-    if isinstance(prompt_ret, str):
-        messages = []
-        if prompt.__doc__ and prompt.__doc__.strip():
-            messages.append({"role": "system", "content": prompt.__doc__})
-        messages.append({"role": "user", "content": prompt_ret})
-        return messages
-    elif isinstance(prompt_ret, list):
-        return prompt_ret
-    else:
-        raise TypeError("Expected prompt_ret to be a str or list of Messages")
--- src/uglychain/response_format.py
+++ src/uglychain/response_format.py
@@ -9,7 +9,10 @@ from pydantic import BaseModel, ValidationError
 # 创建一个泛型变量，用于约束BaseModel的子类
 T = TypeVar("T", bound=BaseModel)
 
-PROMPT_TEMPLATE = """The output should be formatted as a JSON instance that conforms to the JSON schema below.
+
+class ResponseFormatter:
+    # 定义输出格式的提示模板，包含对JSON schema的描述和示例
+    PROMPT_TEMPLATE = """The output should be formatted as a JSON instance that conforms to the JSON schema below.
 
 As an example, for the schema {{"properties": {{"foo": {{"title": "Foo", "description": "a list of strings", "type": "array", "items": {{"type": "string"}}}}}}, "required": ["foo"]}}
 the object {{"foo": ["bar", "baz"]}} is a well-formatted instance of the schema. The object {{"properties": {{"foo": ["bar", "baz"]}}}} is not well-formatted.
@@ -21,38 +24,69 @@ Here is the output schema:
 
 Ensure the response can be parsed by Python json.loads"""
 
+    @staticmethod
+    def get_response_format_prompt(model: type[T]) -> str:
+        """
+        生成模型的响应格式提示。
 
-def get_response_format_prompt(model: type[BaseModel]) -> str:
-    """
-    生成模型的响应格式提示。
+        Args:
+            model (type[T]): 需要生成提示的模型类。
 
-    # Args:
-        model (BaseModel): 需要生成提示的模型实例。
+        Returns:
+            str: 返回响应格式提示字符串。
+        """
+        # 获取模型的JSON schema
+        schema = model.model_json_schema()
+        # 移除不必要的字段，减少提示的冗余信息
+        reduced_schema = schema.copy()
+        if "title" in reduced_schema:
+            del reduced_schema["title"]
+        if "type" in reduced_schema:
+            del reduced_schema["type"]
+        # 将简化后的schema转换为JSON字符串
+        prompt = json.dumps(reduced_schema, ensure_ascii=False)
+        # 格式化并返回提示字符串
+        return ResponseFormatter.PROMPT_TEMPLATE.format(model_json_output_prompt=prompt)
 
-    # Returns:
-        str: 返回响应格式提示字符串。
-    """
-    global PROMPT_TEMPLATE
-    schema = model.model_json_schema()
-    # Remove extraneous fields.
-    reduced_schema = schema
-    if "title" in reduced_schema:
-        del reduced_schema["title"]
-    if "type" in reduced_schema:
-        del reduced_schema["type"]
-    prompt = json.dumps(reduced_schema, ensure_ascii=False)
-    return PROMPT_TEMPLATE.format(model_json_output_prompt=prompt)
+    @staticmethod
+    def parse_model_from_response(model: type[T], response: str) -> T:
+        """
+        从响应字符串中解析并验证模型实例。
 
+        Args:
+            model (type[T]): 模型类，用于解析和验证响应。
+            response (str): 响应字符串，可能包含JSON格式的数据。
 
-def from_response(model: type[T], response: str) -> T:
-    try:
-        match = re.search(r"(\{.*\})", response.strip(), re.MULTILINE | re.IGNORECASE | re.DOTALL)
-        json_str = ""
-        if match:
-            json_str = match.group()
-        return model.model_validate_json(json_str)
+        Returns:
+            T: 解析并验证后的模型实例。
 
-    except (json.JSONDecodeError, ValidationError) as e:
-        name = model.__name__
-        msg = f"Failed to parse {name} from completion {response}. Got: {e}"
-        raise ValueError(msg) from e
+        Raises:
+            ValueError: 如果解析或验证失败，抛出此异常。
+        """
+        try:
+            # 尝试直接解析整个响应字符串
+            json_obj = json.loads(response.strip())
+            return model.model_validate_json(json.dumps(json_obj))
+
+        except json.JSONDecodeError:
+            # 如果直接解析失败，尝试使用正则表达式提取 JSON 字符串
+            match = re.search(r"(\{.*\})", response.strip(), re.MULTILINE | re.IGNORECASE | re.DOTALL)
+            if match:
+                json_str = match.group()
+                try:
+                    return model.model_validate_json(json_str)
+                except (json.JSONDecodeError, ValidationError) as e:
+                    # 如果解析或验证失败，记录错误信息并抛出异常
+                    name = model.__name__
+                    msg = f"Failed to parse {name} from completion {response}. Got: {e}"
+                    raise ValueError(msg) from e
+            else:
+                # 如果正则表达式匹配失败，抛出异常
+                name = model.__name__
+                raise ValueError(f"Failed to find JSON object in response for {name}: {response}") from None
+
+        except ValidationError as e:
+            # 如果验证失败，记录错误信息并抛出异常
+            name = model.__name__
+            msg = f"Failed to validate {name} from completion {response}. Got: {e}"
+            raise ValueError(msg) from e
