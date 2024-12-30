e49fd1d050af4a5d10886d7eb184612ed36d9898
Uglyboy <25730357+uglyboy-tl@users.noreply.github.com>
2024-12-30 19:49:28 +0800

    refactor(map): 重构 map 示例并添加新功能
    - 新增 map 和 map_input 函数，演示了如何使用 LLM 进行映射操作
    - 添加 schema.py 文件，定义了常用的模型和枚举类
    - 更新 test 函数，使用新的 UserDetail 模型
    - 优化代码结构，提高可读性和可维护性

--- examples/map.py
+++ examples/map.py
@@ -2,33 +2,54 @@ from __future__ import annotations
 
 from enum import Enum, unique
 
+from examples.schema import AUTHOR, UserDetail
 from pydantic import BaseModel
 
-from uglychain import config, llm
-from uglychain.response_format import ResponseModel
+from uglychain import config, llm, response_format
 
 
-@unique
-class Gender(Enum):
-    FEMALE = "FEMALE"
-    MALE = "MALE"
+def map():
+    @llm("openai:gpt-4o-mini", map_keys=["input"])
+    def _map(input: list[str]):
+        return input
+
+    input = [
+        "How old are you?",
+        "What is the meaning of life?",
+        "What is the hottest day of the year?",
+    ]
+    for item in _map(input):
+        print(item)
 
 
-class UserDetail(BaseModel):
-    name: str
-    gender: Gender
+def map_input():
+    @llm("openai:gpt-4o-mini", map_keys=["book"], response_format=AUTHOR)
+    def _map(book: list[str], position: str):
+        return f"{book}的{position}是谁？"
+
+    input = [
+        "《红楼梦》",
+        "《西游记》",
+        "《三国演义》",
+        "《水浒传》",
+    ]
+    for item in _map(book=input, position="作者"):
+        print(item)
 
 
-def fc(name: str) -> str:
-    return f"{name} is a boy"
+def test():
+    @llm("openai:gpt-4o-mini", map_keys=["input"], response_format=UserDetail)
+    def _map(input: list[str]):
+        return f"{input}"
 
-
-@llm("openai:gpt-4o-mini", map_keys=["name"], response_format=UserDetail)
-def test(gender: str, name: list[str]):
-    return f"{name} is a {gender}"
+    input = ["Extract Jason is a boy", "Extract Jason is a girl", "Extract Robin is a boy", "Extract Misty is a girl"]
+    for item in _map(input):
+        print(item)
 
 
 if __name__ == "__main__":
-    config.verbose = True
+    # config.verbose = True
     config.use_parallel_processing = True
-    test("boy", ["Alice", "Bob", "Uglyboy"])
+    # map()
+    map_input()
+    # test()
---
+++ examples/schema.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from enum import Enum, unique
+
+from pydantic import BaseModel, Field
+
+
+@unique
+class Gender(Enum):
+    FEMALE = "FEMALE"
+    MALE = "MALE"
+
+
+class UserDetail(BaseModel):
+    name: str
+    gender: Gender
+
+
+class AUTHOR(BaseModel):
+    name: str = Field(..., description="姓名")
+    introduction: str = Field(..., description="简介")
+
+
+class Label(Enum):
+    COUNTRY = "国家"
+    CITY = "城市"
+    MOVIE = "电影"
+    TV = "电视剧"
+    PERSON = "人物"
